71b99e9ed7ab4e6bbe1bbb16e1e6cb4875480f7e
Bart-Zw <9007171@student.zadkine.nl>
2022-06-07 11:45:24 +0200

    push

---
+++ index.js
@@ -0,0 +1,6 @@
+//laat de canvas zien op de browers
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+//maakt de canavas breeder
+canvas.width = innerWidth
+canvas.height = innerHeight
