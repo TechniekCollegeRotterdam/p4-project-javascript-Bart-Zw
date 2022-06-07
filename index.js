7b27195fa713989b807dc562ed4b7e8fa62e1700
Bart-Zw <9007171@student.zadkine.nl>
2022-06-07 12:09:34 +0200

    space invanders begin

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 //laat de canvas zien op de browers
+
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 //maakt de canavas breeder
+
 canvas.width = innerWidth
 canvas.height = innerHeight
