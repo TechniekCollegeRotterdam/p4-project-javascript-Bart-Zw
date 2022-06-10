f53a26fcf73068444c84f6d24778ff81d3d9554a
Bart-Zw <9007171@student.zadkine.nl>
2022-06-10 11:52:01 +0200

    game verandered

--- index.js
+++ index.js
@@ -1,151 +1,99 @@
-//laat de canvas zien op de browers
-const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d')
-//maakt de canavas breeder
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
 
-canvas.width = innerWidth
-canvas.height = innerHeight
-//speler aan maken
-class Player{
-    constructor() {
+canvas.width = innerWidth;
+canvas.height = innerHeight;
 
+class Player {
+  constructor() {
+    this.velocity = {
+      x: 0,
+      y: 0,
+    };
+    const image = new Image();
+    image.src = "./img/spaceship.png";
+    image.onload = () => {
+      const scale = 0.15;
+      this.image = image;
+      this.width = image.width * scale;
+      this.height = image.height * scale;
       this.position = {
-        x: 200,
-        y: 100
-      }
-      
-         
-        this.velocity = {
-            x: 0,
-            y: 0
-        }
+        x: canvas.width / 2 - this.width / 2,
+        y: canvas.height - this.height,
+      };
+    };
+  }
 
-         this.rotation = 0
+  draw() {
+    c.drawImage(
+      this.image,
+      this.position.x,
+      this.position.y,
+      this.width,
+      this.height
+    );
+  }
 
-        //speler onder in het schrem te krijgen
-        const image = new Image()
-        image.src = './img/spaceship.png'
-        image.onload = () => {
-            const scale = 0.15
-            this.image = image
-            this.width = image.width * scale  
-            this.height = image.height * scale
-            this.position = {
-                x: canvas.width / 2 - this.width / 2,
-                y: canvas.height - this.height - 20
-            }
-        }
-
-        
+  update() {
+    if (this.image) {
+      this.draw();
+      this.position.x += this.velocity.x;
     }
-    //de speler laten verschijnen op het scherm
-    draw() {
-        //c.fillStyle = 'red'
-        //c.fillRect(this.position.x, this.position.y, this.width, 
-        //this.height) 
-
-        //speler kantelt mee als je naar links of rechts ga
-        c.save()
-        c.translate(
-        player.position.x + player.width / 2,
-        player.position.y + player.height / 2)
-
-
-        c.rotate(this.rotation)
-
-        c.translate(
-            -player.position.x - player.width / 2,
-            -player.position.y - player.height / 2)
-
-        if(this.image)
-        c.drawImage(
-            this.image, 
-            this.position.x, 
-            this.position.y, 
-            this.width, 
-            this.height
-            )    
-            c.restore
-        }
-
-    update(){
-        if (this,this.image){
-            this.draw
-        this.position.x += this.velocity.x
-        }
-    }
+  }
 }
 
-const player = new Player()
+const player = new Player();
 const keys = {
-    a: {
-        pressed: false
-    },
-    d: {
-        pressed: false
-    },
-    space: {
-        pressed: false
-    }
-}
+  a: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+  space: {
+    pressed: false,
+  },
+};
 
 function animate() {
-    requestAnimationFrame(animate)
-    player.draw();
-    c.fillStyle = 'black'
-    c.fillRect(0, 0, canvas.width, canvas.height)
-    player.update()
-    
-    if (keys.a.pressed && player.position.x >=0) {
-        player.velocity.x = -7
-        player.rotation = -0.15
-    } else if(keys.d.pressed && player.position.x +player.width <= canvas.width){
-        player.velocity.x = 7
-        player.rotation = 0.15
-
-    } else{
-        player.velocity.x = 0
-        player.rotation = 0
-
-    }
+  requestAnimationFrame(animate);
+  c.fillStyle = "black";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  player.update();
+  // als je op A druk dan ga je naar links
+  if (keys.a.pressed) {
+    player.velocity.x = -5;
+    // als je op D druk dan ga je naar rechts
+  } else if (keys.d.pressed) {
+    player.velocity.x = 5
+  } else {
+    player.velocity.x = 0;
+  }
 }
+animate();
 
-animate() 
-//speler laten bewegen met A.S.D.w
-addEventListener('keydown', ({key}) => {
-    console.log()
-    switch (key) {
-        case 'a':
-            console.log('left')
-            player.velocity.x = -5
-            keys.a.pressed = true
-            break
-            case 'd':
-            console.log('right')
-            keys.d.pressed = true
-            break
-            case '':
-            console.log('space')
-            break
-    }
-})
+addEventListener("keydown", ({ key }) => {
+  switch (key) {
+    case "a":
+      keys.a.pressed = true;
+      break;
+    case "d":
+      keys.d.pressed = true;
+      break;
+    case " ":
+      break;
+  }
+});
 
-
-
-addEventListener('keyup', ({key}) => {
-    console.log()
-    switch (key) {
-        case 'a':
-            console.log('left')
-            player.velocity.x = -5
-            keys.a.pressed = false
-            break
-            case 'd':
-            console.log('right')
-            keys.d.pressed = false
-            break
-            case '':
-            console.log('space')
-            break
-    }
-})
+addEventListener("keyup", ({ key }) => {
+  switch (key) {
+    case "a":
+      keys.a.pressed = false;
+      break;
+    case "d":
+      keys.d.pressed = false;
+      break;
+    case " ":
+      break;
+  }
+});
